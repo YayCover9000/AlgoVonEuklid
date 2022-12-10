9ee835cc03bbf33980fae999511674a187506fa2
YayCover9000 <yassuanf@googlemail.com>
2022-12-10 16:44:44 +0100

    Program sucsessfull written

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int rest=1;
+        int y;
+        int x;
+        int groessereZahl;
+        int kleinereZahl;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ganze zahl 1");
+        x = sc.nextInt();
+        System.out.println("ganze zahl 2");
+        y = sc.nextInt();
+        if(x>y){
+            groessereZahl = x;
+            kleinereZahl = y;
+        }else {
+            groessereZahl = y;
+            kleinereZahl = x;
+        }
+        System.out.println(groessereZahl);
+        while(rest>0) {
+            rest = groessereZahl % kleinereZahl;
+            System.out.println(rest);
+            groessereZahl = kleinereZahl;
+            if(rest==0) {
+                System.out.println("ggT ist: " + kleinereZahl);
+                return;
+            }
+            kleinereZahl=rest;
+            System.out.println(rest);
+
+
+        }
+
+
     }
 }
